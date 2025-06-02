08ef092290c76e08289c0d4453af55bd20a7f032
Damith Premasiri <damithpremasiri@gmail.com>
2025-06-02 21:22:54 +0100

    anonymise slurm

--- batch_anonymise.sh
+++ batch_anonymise.sh
@@ -1,10 +1,9 @@
+#!/bin/bash
 #SBATCH --partition=cpu-6h
 #SBATCH --output=log/output_%A_%a.log
 #SBATCH --array=0-15
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=${SLURM_EMAIL}
-
-set -x
 
 source venv/bin/activate
 
