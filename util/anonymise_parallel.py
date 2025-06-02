d87ffceeeac4ead7e9e64f702dc81cd8c37bb0e6
Damith Premasiri <damithpremasiri@gmail.com>
2025-06-02 18:50:58 +0100

    anonymise

--- util/anonymise_parallel.py
+++ util/anonymise_parallel.py
@@ -53,7 +53,7 @@ if __name__ == '__main__':
     os.makedirs(anon_path, exist_ok=True)
 
     files = [f for f in os.listdir(files_path) if f.endswith('.json')]
-    workers = 50
+    workers = 4
     # Initialize shared nlp pool
     init_nlp_pool(size=workers)
 
