d2b22d6e3f4fff289e0531c10c5016de042e2e4b
Damith Premasiri <damithpremasiri@gmail.com>
2025-10-22 18:38:36 +0100

    modern bert

--- scripts/train_sentence_embeddings.sh
+++ scripts/train_sentence_embeddings.sh
@@ -4,7 +4,7 @@
 
 source venv/bin/activate
 
-python3 -m experiments.training.single_paragraph_transformer --model_name=answerdotai/ModernBERT-base \
+python3 -m experiments.training.single_paragraph_transformer --model_name=nlpaueb/legal-bert-base-uncased \
 --training_file_path=data/data_splits/training/ --training_file=anchor_positive_W1.tsv \
 --eval_file_path=data/data_splits/training/ --eval_file=eval_positive_negative_W1.tsv \
 --run_alias=positive_negative_W1 --batch_size=16 --eval_batch_size=16
