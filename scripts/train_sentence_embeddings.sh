67d68fdfe6d65f1d5e3c9264aefce976bb204c57
Damith Premasiri <damithpremasiri@gmail.com>
2025-10-22 16:23:54 +0100

    change batch size 1

--- scripts/train_sentence_embeddings.sh
+++ scripts/train_sentence_embeddings.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 #SBATCH --partition=a5000-48h
+#SBATCH --mem-per-gpu=24G
+#SBATCH --gres=gpu:1
 #SBATCH --output=log/output_%A_%a.log
 
 source venv/bin/activate
@@ -7,4 +9,4 @@ source venv/bin/activate
 python3 -m experiments.training.single_paragraph_transformer --model_name=nlpaueb/legal-bert-base-uncased \
 --training_file_path=data/data_splits/training/ --training_file=anchor_positive_W1.tsv \
 --eval_file_path=data/data_splits/training/ --eval_file=eval_positive_negative_W1.tsv \
---run_alias=positive_negative_W1 --batch_size=1 --eval_batch_size=1
+--run_alias=positive_negative_W1 --batch_size=16 --eval_batch_size=16
