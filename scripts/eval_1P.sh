8b833d7ea1adac2139e8de8605e30b3b64117d7a
Damith Premasiri <damithpremasiri@gmail.com>
2025-10-29 15:13:50 +0000

    run_shell

---
+++ scripts/eval_1P.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --partition=a5000-48h
+#SBATCH --exclude=hex-p3-g19
+#SBATCH --output=log/output_%A_%a.log
+#SBATCH --mem=40G
+
+source venv/bin/activate
+
+python -m experiments.evaluation.single_para_eval \
+--model_path models/nlpaueb_legal-bert-base-uncased_positive_negative_W1 \
+--candidates_file_path data/data_splits/ --gold_file_path data/data_splits/ \
+--candidates_file candidates_1P.tsv --gold_file gold_1P.tsv --run_alias 1P_eval
