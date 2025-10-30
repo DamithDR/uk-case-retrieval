6841c0441789d4faef21814a3b4ed6a7ee08522f
Damith Premasiri <damithpremasiri@gmail.com>
2025-10-30 12:54:14 +0000

    eval

--- scripts/eval_1P.sh
+++ scripts/eval_1P.sh
@@ -7,6 +7,6 @@
 source venv/bin/activate
 
 python -m experiments.evaluation.single_para_eval \
---model_name models/google-bert_bert-base-uncased_positive_negative_W1/final/ \
+--model_name models/google-bert_bert-base-uncased_positive_negative_W3/final/ \
 --candidates_file_path data/data_splits/ --gold_file_path data/data_splits/ \
---candidates_file candidates_1P.tsv --gold_file gold_1P.tsv --run_alias 1P_eval
+--candidates_file candidates_3P.tsv --gold_file gold_3P.tsv --run_alias 3P_eval
