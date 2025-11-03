22e8ec5769ae3b2b40a117c6f96c21839ccc75f2
Damith Premasiri <damithpremasiri@gmail.com>
2025-11-03 17:51:56 +0000

    qwen

--- scripts/train_sentence_embeddings.sh
+++ scripts/train_sentence_embeddings.sh
@@ -6,7 +6,7 @@
 
 source venv/bin/activate
 
-python3 -m experiments.training.single_paragraph_transformer --model_name=answerdotai/ModernBERT-base \
+python3 -m experiments.training.single_paragraph_transformer --model_name=Qwen/Qwen2.5-1.5B \
 --training_file_path=data/data_splits/training/ --training_file=anchor_positive_W1.tsv \
 --eval_file_path=data/data_splits/training/ --eval_file=eval_positive_negative_W1.tsv \
 --run_alias=positive_negative_W1 --batch_size=2 --eval_batch_size=2
