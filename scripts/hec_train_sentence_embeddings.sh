d98d71fb338f990c5ef4329f5e4b152bac2907e4
Damith Premasiri <damithpremasiri@gmail.com>
2025-11-21 22:00:53 +0000

    qwen 4b

--- scripts/hec_train_sentence_embeddings.sh
+++ scripts/hec_train_sentence_embeddings.sh
@@ -6,8 +6,8 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=d.dolamullage@lancaster.ac.uk
-#SBATCH --output=/storage/hpc/41/dolamull/experiments/uk-case-retrieval/qwen4_output.log
-#SBATCH --error=/storage/hpc/41/dolamull/experiments/uk-case-retrieval/qwen4_error.log
+#SBATCH --output=/storage/hpc/41/dolamull/experiments/uk-case-retrieval/jina_output.log
+#SBATCH --error=/storage/hpc/41/dolamull/experiments/uk-case-retrieval/jina_error.log
 
 source /etc/profile
 module add anaconda3/2023.09
@@ -27,7 +27,7 @@ done < .env
 
 huggingface-cli login --token $HUGGINGFACE_TOKEN
 
-python -m experiments.training.single_paragraph_transformer --model_name=Qwen/Qwen3-Embedding-4B \
+python -m experiments.training.single_paragraph_transformer --model_name=jinaai/jina-embeddings-large-v2 \
 --training_file_path=data/data_splits/training/ --training_file=anchor_positive_W1.tsv \
 --eval_file_path=data/data_splits/training/ --eval_file=eval_positive_negative_W1.tsv \
 --run_alias=positive_negative_W1 --batch_size=2 --eval_batch_size=2
