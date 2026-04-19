0faab77ec4748ea8e635d48055cd422568b51cab
Damith Premasiri <damithpremasiri@gmail.com>
2026-04-19 11:44:18 +0100

    extend eval 1P

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 #SBATCH --partition=a5000-48h
-#SBATCH --exclude=hex-p3-g19
+#SBATCH --gres=gpu:nvidia_rtx_a5000:1
 #SBATCH --output=log/output_%A_%a.log
 #SBATCH --mem=40G
+
+if [ -z "$1" ]; then
+    echo "Usage: sbatch scripts/eval.sh <model_name>"
+    exit 1
+fi
+
+MODEL_NAME="$1"
 
 source venv/bin/activate
 
 python -m experiments.evaluation.single_para_eval \
---model_name models/google-bert_bert-base-uncased_positive_negative_W3/final/ \
+--model_name "$MODEL_NAME" \
 --candidates_file_path data/data_splits/ --gold_file_path data/data_splits/ \
---candidates_file candidates_3P.tsv --gold_file gold_3P.tsv --run_alias 3P_eval
+--candidates_file candidates_1P.tsv --gold_file gold_1P.tsv --run_alias 1P_eval
